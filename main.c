2090a858c8d9f9039166c73a0bdefb553f93d59c
Rafael Lopes <faelzim34@gmail.com>
2017-05-21 04:48:23 -0300

    correção

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void converterBaseX(){
+void converterBase(){
     
     int * vetor = malloc(sizeof(int) * 100);
     int i,j,n,X;
@@ -9,37 +9,39 @@ void converterBaseX(){
     
     printf("\n");
     printf("\t=================================\n");
-    printf("\t|      Conversor de Base X      |\n");
+    printf("\t|       Conversor de Base       |\n");
     printf("\t=================================\n\n");
+    
     printf("\nInforme o numero da base 10 que deseja converter: ");
     scanf("%d", &n);
     n_aux=n;
-    printf("Informe um numero [1-9] de base que deseja converter o numero %d: ",n);
+    
+    printf("Informe um numero [1-10] de base que deseja converter o numero %d: ",n);
     scanf("%d", &X);
     
-    while(X<1 || X>10){
+    while(X<1 || X>10){ //nao aceita valores menores que 1 e maiores que 10
         
-        printf("NUMERO INVALIDO - Informe um numero [1-9] de base que deseja converter o numero %d: ",n);
+        printf("NUMERO INVALIDO - Informe um numero [1-10] de base que deseja converter o numero %d: ",n);
         scanf("%d", &X);
         
     }
-    if(X==1 || X==10){
+    if(X==1 || X==10){ //mesmo valor
         
-        printf("O numero %d na base 10, convertido para a base %d, eh: %d",n_aux,X,n);
+        printf("O numero %d na base 10, convertido para a base %d, eh: %d",n_aux,X,n); //assume que o valor convertido é o mesmo recebido
         
     }
     else{
     
         while(n>=1){
-            vetor[i]= (n%X);
-            n=n/X;
-            i++;
+            vetor[i]= (n%X); //vetor recebe o resto da divisao pela base (numero convertido)
+            n=n/X; //proximo numero a ser dividido eh o quociente inteiro da divisao
+            i++; //acrescenta 1 unidade ao vetor
         }
 
         printf("O numero %d na base 10, convertido para a base %d, eh: ",n_aux,X);
 
-        for(j=0;i>0;i--){
-            printf("%d", vetor[i-1]);
+        for(i=i;i>0;i--){ //condicao para ler vetor de traz pra frente
+            printf("%d ", vetor[i-1]);
 
         }
     }
@@ -48,9 +50,7 @@ void converterBaseX(){
 
 int main(int argc, char** argv) {
         
-    converterBaseX();
-
+    converterBase();
 
     return (EXIT_SUCCESS);
 }
-
